e5ba272e72e344cbf180243727ab3289b0a07b64
marcel <marcel@192.168.1.24>
2017-12-15 21:43:42 +0100

    day1

---
+++ src/com/adventofcode/day1/Main.java
@@ -0,0 +1,33 @@
+package com.adventofcode.day1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		String outString, suba, subb;
+		int wynik;
+		Scanner in = new Scanner(new FileReader("D:\\cyfry.txt"));
+		StringBuilder sb = new StringBuilder();
+		while(in.hasNext()) {
+		    sb.append(in.next());
+		}
+		in.close();
+		outString = sb.toString();
+		wynik = 0;
+		for (int i = 0; i < outString.length()-1; i++) {
+			suba = outString.substring(i, i+1);
+			subb = outString.substring(i+1, i+2);
+			if (Integer.parseInt(suba) == Integer.parseInt(subb))
+				wynik += Integer.parseInt(suba);
+		}
+		suba = outString.substring(0, 1);
+		subb = outString.substring(outString.length()-1, outString.length());
+		if (Integer.parseInt(suba) == Integer.parseInt(subb))
+			wynik += Integer.parseInt(suba);
+		 System.out.println(wynik);
+	}
+
+}
